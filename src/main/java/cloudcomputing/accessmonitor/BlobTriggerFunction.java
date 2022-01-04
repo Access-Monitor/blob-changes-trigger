cf67940eb5fa9034b1ed0b4e70e3eeff882d6f51
leo-capvano <leocapuano0@gmail.com>
2022-01-04 22:31:55 +0100

    Initial commit

---
+++ src/main/java/cloudcomputing/accessmonitor/BlobTriggerFunction.java
@@ -0,0 +1,22 @@
+package cloudcomputing.accessmonitor;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Azure Blob trigger.
+ */
+public class BlobTriggerFunction {
+    /**
+     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
+     */
+    @FunctionName("BlobTrigger-Java")
+    @StorageAccount("MyStorageConnectionAppSetting")
+    public void run(
+            @BlobTrigger(name = "content", path = "accessmonitorblob/{filename}", dataType = "binary") byte[] content,
+            @BindingName("filename") String filename,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Blob trigger function processed a blob. Name: " + filename + "\n  Size: " + content.length + " Bytes");
+    }
+}
