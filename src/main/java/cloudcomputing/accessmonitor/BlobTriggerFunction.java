76a137095cabe6210547dea89ad89d42106092f1
leo-capvano <leocapuano0@gmail.com>
2022-01-05 17:25:04 +0100

    added face detection from blob content

--- src/main/java/cloudcomputing/accessmonitor/BlobTriggerFunction.java
+++ src/main/java/cloudcomputing/accessmonitor/BlobTriggerFunction.java
@@ -1,22 +1,59 @@
 package cloudcomputing.accessmonitor;
 
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.functions.*;
+import com.google.gson.Gson;
+import com.microsoft.azure.cognitiveservices.vision.faceapi.models.DetectedFace;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.BlobTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.StorageAccount;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
-/**
- * Azure Functions with Azure Blob trigger.
- */
 public class BlobTriggerFunction {
-    /**
-     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
-     */
-    @FunctionName("BlobTrigger")
-    @StorageAccount("AzureWebJobsStorage")
-    public void run(
-            @BlobTrigger(name = "content", path = "accessmonitorblob/{filename}", dataType = "binary") byte[] content,
-            @BindingName("filename") String filename,
-            final ExecutionContext context
-    ) {
-        context.getLogger().info("Java Blob trigger function processed a blob. Name: " + filename + "\n  Size: " + content.length + " Bytes");
+
+  public static final int SUCCESS = 200;
+  public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+  public static final String FACE_API_ENDPOINT = "FaceAPIEndpoint";
+  public static final String FACE_API_SUBSCRIPTION_KEY = "FaceAPISubscriptionKey";
+
+  @FunctionName("BlobTrigger")
+  @StorageAccount("AzureWebJobsStorage")
+  public void run(@BlobTrigger(name = "blobContent", path = "accessmonitorblob/{filename}", dataType = "binary") byte[] blobContent,
+    @BindingName("filename") String filename, final ExecutionContext context) {
+
+    context.getLogger()
+      .info("Java Blob trigger function processed a blob. Name: " + filename + "\n  Size: " + blobContent.length + " Bytes");
+
+    try {
+      HttpResponse<String> httpResponse = faceDetect(blobContent);
+
+      if (httpResponse.statusCode() == SUCCESS) {
+        DetectedFace[] detectedFaces = new Gson().fromJson(httpResponse.body(), DetectedFace[].class);
+
+      }
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
     }
+
+  }
+
+  private HttpResponse<String> faceDetect(byte[] blobContent) throws IOException, InterruptedException {
+    HttpClient httpClient = HttpClient.newHttpClient();
+    HttpRequest httpRequest = buildRequest(blobContent);
+    return httpClient.send(httpRequest, BodyHandlers.ofString());
+  }
+
+  private HttpRequest buildRequest(byte[] blobContent) {
+    return HttpRequest.newBuilder(URI.create(System.getenv(FACE_API_ENDPOINT)))
+             .header("Content-type", APPLICATION_OCTET_STREAM)
+             .header("Ocp-Apim-Subscription-Key", System.getenv(FACE_API_SUBSCRIPTION_KEY))
+             .POST(BodyPublishers.ofByteArray(blobContent))
+             .build();
+  }
 }
