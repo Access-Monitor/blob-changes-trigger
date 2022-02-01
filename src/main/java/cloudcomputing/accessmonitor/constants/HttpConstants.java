1b8db0a17e8927efe0b45c5ac31881066ca64159
leo-capvano <leocapuano0@gmail.com>
2022-02-01 22:27:24 +0100

    cleanup

--- src/main/java/cloudcomputing/accessmonitor/constants/HttpConstants.java
+++ src/main/java/cloudcomputing/accessmonitor/constants/HttpConstants.java
@@ -6,6 +6,5 @@ public class HttpConstants {
   public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
   public static final String OCP_APIM_SUBSCRIPTION_KEY_HEADER = "Ocp-Apim-Subscription-Key";
   public static final String CONTENT_TYPE_HEADER = "Content-type";
-  public static final String AMPERSAND = "&";
 
 }
